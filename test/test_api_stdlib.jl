0c9815ce0f839ae97a7bc4bff0eee89c39634018
Mandar Chitre <mandar@nus.edu.sg>
2025-02-03 00:29:06 +0800

    test: rework test cases with VSCode Julia test framework (WIP)

---
+++ test/test_api_stdlib.jl
@@ -0,0 +1,172 @@
+using TestItems
+
+@testsnippet PekerisSetup begin
+  env = UnderwaterEnvironment(
+    bathymetry = 20.0u"m",
+    temperature = 27.0u"°C",
+    salinity = 35.0u"ppt",
+  )
+end
+
+@testitem "env" setup=[PekerisSetup] begin
+  for e ∈ (env, PekerisWaveguide(h=20.0u"m"))
+    @test e isa UnderwaterEnvironment
+    @test UnderwaterAcoustics.is_range_dependent(e) == false
+    @test UnderwaterAcoustics.isospeed(e) == true
+    @test e.bathymetry == 20.0
+    @test e.altimetry == 0.0
+    @test e.temperature == 27.0
+    @test e.salinity == 35.0
+    @test e.soundspeed ≈ 1539.0 atol=0.1
+    @test e.density ≈ 1022.7 atol=0.1
+    @test e.surface == PressureReleaseBoundary
+    @test e.seabed == RigidBoundary
+  end
+end
+
+@testitem "src" begin
+  src = AcousticSource(-5.0, 1000.0, 180.0)
+  @test src isa UnderwaterAcoustics.AbstractAcousticSource
+  @test location(src) == (x=0.0, y=0.0, z=-5.0)
+  @test frequency(src) == 1000.0
+  @test spl(src) == 180.0
+  src = AcousticSource((10.0, -5.0), 1000.0)
+  @test location(src) == (x=10.0, y=0.0, z=-5.0)
+  @test frequency(src) == 1000.0
+  @test spl(src) == 0.0
+  src = AcousticSource((10.0, 20.0, -5.0), 1000.0)
+  @test location(src) == (x=10.0, y=20.0, z=-5.0)
+  @test frequency(src) == 1000.0
+  @test spl(src) == 0.0
+  src = AcousticSource(-5u"m", 1u"kHz", 180u"dB")
+  @test location(src) == (x=0.0, y=0.0, z=-5.0)
+  @test frequency(src) == 1000.0
+  @test spl(src) == 180.0
+  src = AcousticSource((10u"m", -5u"m"), 1000.0)
+  @test location(src) == (x=10.0, y=0.0, z=-5.0)
+  @test frequency(src) == 1000.0
+  @test spl(src) == 0.0
+  src = AcousticSource((10u"m", 20u"m", -5u"m"), 1000.0)
+  @test location(src) == (x=10.0, y=20.0, z=-5.0)
+  @test frequency(src) == 1000.0
+  src = AcousticSource((x=10u"m", y=20u"m", z=-5u"m"), 1u"kHz")
+  @test location(src) == (x=10.0, y=20.0, z=-5.0)
+  @test frequency(src) == 1000.0
+  src = AcousticSource(nothing, 1000.0)
+  @test location(src) === nothing
+  @test frequency(src) == 1000.0
+  src = AcousticSource(missing, 1000.0)
+  @test location(src) === missing
+  @test frequency(src) == 1000.0
+end
+
+@testitem "rcv" begin
+  rcv = AcousticReceiver(10.0, 20.0, 0.0)
+  @test rcv isa UnderwaterAcoustics.AbstractAcousticReceiver
+  @test location(rcv) == (x=10.0, y=20.0, z=0.0)
+  rcv = AcousticReceiver((10.0, 20.0, 0.0))
+  @test location(rcv) == (x=10.0, y=20.0, z=0.0)
+  rcv = AcousticReceiver(10.0, -20.0)
+  @test location(rcv) == (x=10.0, y=0.0, z=-20.0)
+  rcv = AcousticReceiver((10.0, -20.0))
+  @test location(rcv) == (x=10.0, y=0.0, z=-20.0)
+  rcv = AcousticReceiver(-20.0)
+  @test location(rcv) == (x=0.0, y=0.0, z=-20.0)
+  rcv = AcousticReceiver(10u"m", 20u"m", 0u"m")
+  @test location(rcv) == (x=10.0, y=20.0, z=0.0)
+  rcv = AcousticReceiver((10u"m", 20u"m", 0u"m"))
+  @test location(rcv) == (x=10.0, y=20.0, z=0.0)
+  rcv = AcousticReceiver(10u"m", -20u"m")
+  @test location(rcv) == (x=10.0, y=0.0, z=-20.0)
+  rcv = AcousticReceiver((10u"m", -20u"m"))
+  @test location(rcv) == (x=10.0, y=0.0, z=-20.0)
+  rcv = AcousticReceiver(-20u"m")
+  @test location(rcv) == (x=0.0, y=0.0, z=-20.0)
+end
+
+@testitem "rcv-grid" begin
+  rcv = AcousticReceiverGrid2D(0.0:10.0:1000.0, -20.0:0.0)
+  @test rcv isa AbstractMatrix{AcousticReceiver}
+  @test size(rcv) == (101, 21)
+  @test location(rcv[1, 1]) == (x=0.0, y=0.0, z=-20.0)
+  @test location(rcv[101, 21]) == (x=1000.0, y=0.0, z=0.0)
+  rcv = AcousticReceiverGrid2D(0u"m":10u"m":1u"km", -20u"m":1u"m":0u"m")
+  @test size(rcv) == (101, 21)
+  @test location(rcv[1, 1]) == (x=0.0, y=0.0, z=-20.0)
+  @test location(rcv[101, 21]) == (x=1000.0, y=0.0, z=0.0)
+  rcv = AcousticReceiverGrid3D(0.0:10.0:1000.0, -100:100, -20.0:0.0)
+  @test rcv isa AbstractArray{AcousticReceiver,3}
+  @test size(rcv) == (101, 201, 21)
+  @test location(rcv[1, 1, 1]) == (x=0.0, y=-100.0, z=-20.0)
+  @test location(rcv[101, 201, 21]) == (x=1000.0, y=100.0, z=0.0)
+  rcv = AcousticReceiverGrid3D(0u"m":10u"m":1000u"m", -100u"m":1u"m":100u"m", -20u"m":1u"m":0u"m")
+  @test size(rcv) == (101, 201, 21)
+  @test location(rcv[1, 1, 1]) == (x=0.0, y=-100.0, z=-20.0)
+  @test location(rcv[101, 201, 21]) == (x=1000.0, y=100.0, z=0.0)
+end
+
+@testitem "boundary" begin
+  @test FluidBoundary <: UnderwaterAcoustics.AbstractAcousticBoundary
+  @test PressureReleaseBoundary isa FluidBoundary
+  @test RigidBoundary isa FluidBoundary
+  for sb ∈ (Rock, Pebbles, SandyGravel, CoarseSand, MediumSand, FineSand, VeryFineSand, ClayeySand, CoarseSilt, SandySilt, Silt, FineSilt, SandyClay, SiltyClay, Clay)
+    @test sb isa FluidBoundary
+    @test reflection_coef(sb, 1000.0, 0.1, 1023.0, 1540.0) == reflection_coef(0.1, sb.ρ/1023.0, sb.c/1540.0, sb.δ)
+  end
+  @test WindySurface <: UnderwaterAcoustics.AbstractAcousticBoundary
+  for ss ∈ (SeaState0, SeaState1, SeaState2, SeaState3, SeaState4, SeaState5, SeaState6, SeaState7, SeaState8, SeaState9)
+    @test ss isa WindySurface
+    @test reflection_coef(ss, 1000.0, 0.1, 1023.0, 1540.0) == surface_reflection_coef(ss.windspeed, 1000.0, 0.1)
+  end
+end
+
+@testitem "field" begin
+  fld = SampledField([0.0, 10.0, 0.0]; x=[0.0, 10.0, 20.0])
+  @test fld(0.0) == 0.0
+  @test fld(5.0) == 5.0
+  @test fld(10.0) == 10.0
+  @test fld(15.0) == 5.0
+  @test fld(20.0) == 0.0
+  @test fld((x=0.0, y=0.0, z=0.0)) == 0.0
+  @test fld((x=5.0, y=0.0, z=0.0)) == 5.0
+  @test fld((x=10.0, y=0.0, z=0.0)) == 10.0
+  @test fld((x=15.0, y=0.0, z=0.0)) == 5.0
+  @test fld((x=20.0, y=0.0, z=0.0)) == 0.0
+  @test fld((x=10.0, y=0.0, z=5.0)) == 10.0
+  @test fld((x=10.0, y=5.0, z=0.0)) == 10.0
+  fld = SampledField([0.0, 10.0, 0.0]; z=[0.0, 10.0, 20.0])
+  @test fld(0.0) == 0.0
+  @test fld(5.0) == 5.0
+  @test fld(10.0) == 10.0
+  @test fld(15.0) == 5.0
+  @test fld(20.0) == 0.0
+  @test fld((x=0.0, y=0.0, z=0.0)) == 0.0
+  @test fld((x=0.0, y=0.0, z=5.0)) == 5.0
+  @test fld((x=0.0, y=0.0, z=10.0)) == 10.0
+  @test fld((x=0.0, y=0.0, z=15.0)) == 5.0
+  @test fld((x=0.0, y=0.0, z=20.0)) == 0.0
+  @test fld((x=5.0, y=0.0, z=10.0)) == 10.0
+  @test fld((x=0.0, y=5.0, z=10.0)) == 10.0
+  fld = SampledField([0.0 1.0; 1.0 2.0]; x=[0.0, 1.0], y=[0.0, 1.0])
+  @test fld(0.0, 0.0) == 0.0
+  @test fld(0.5, 0.5) == 1.0
+  @test fld(1.0, 1.0) == 2.0
+  @test fld((x=0.0, y=0.0, z=0.0)) == 0.0
+  @test fld((x=0.5, y=0.5, z=0.0)) == 1.0
+  @test fld((x=1.0, y=1.0, z=0.0)) == 2.0
+  @test fld((x=0.0, y=0.0, z=0.0)) == 0.0
+  @test fld((x=1.0, y=0.0, z=1.0)) == 1.0
+  fld = SampledField([0.0; 1.0;; 1.0; 2.0;;; 0.0; 1.0;; 1.0; 2.0]; x=[0.0, 1.0], y=[0.0, 1.0], z=[0.0, 1.0])
+  @test fld(0.0, 0.0, 0.0) == 0.0
+  @test fld(0.5, 0.5, 0.5) == 1.0
+  @test fld(1.0, 1.0, 1.0) == 2.0
+  @test fld((x=0.0, y=0.0, z=0.0)) == 0.0
+  @test fld((x=0.5, y=0.5, z=0.5)) == 1.0
+  @test fld((x=1.0, y=1.0, z=1.0)) == 2.0
+end
+
+@testitem "noise" begin
+  @test WhiteGaussianNoise <: UnderwaterAcoustics.AbstractNoiseModel
+  @test RedGaussianNoise <: UnderwaterAcoustics.AbstractNoiseModel
+  # TODO: add tests to check that noise models are working as expected
+end

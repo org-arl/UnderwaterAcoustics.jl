9f92174f451281d624af2cd34aff6a32bdae0e84
Mandar Chitre <mandar@nus.edu.sg>
2025-04-24 01:58:16 +0800

    test: disable persistent task check, as precompile seems to take long enough for this to occasionally fail

--- test/test_aqua.jl
+++ test/test_aqua.jl
@@ -3,5 +3,5 @@ using TestItems
 @testitem "aqua" begin
   using UnderwaterAcoustics
   using Aqua
-  Aqua.test_all(UnderwaterAcoustics)
+  Aqua.test_all(UnderwaterAcoustics; persistent_tasks=false)
 end

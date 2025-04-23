9745554389660d9f0b2a916bcac811a332b190b0
Mandar Chitre <mandar@nus.edu.sg>
2025-04-24 01:37:26 +0800

    fix: add chain rule ext to handle mutating impulse response gradient with Zygote

---
+++ ext/UnderwaterAcousticsChainRules.jl
@@ -0,0 +1,33 @@
+module UnderwaterAcousticsChainRules
+
+using ChainRulesCore
+import UnderwaterAcoustics: _arr2ir
+
+function ChainRulesCore.rrule(::typeof(_arr2ir), ts, ϕs; T, t0, fs, n)
+  ir = _arr2ir(ts, ϕs; T, t0, fs, n)
+  # TODO: check if this needs a let block to avoid boxing
+  function _arr2ir_pullback(dx)
+    dx = unthunk(dx)
+    T1 = promote_type(eltype(ts), eltype(dx))
+    T2 = promote_type(eltype(ϕs), eltype(dx))
+    dt = zeros(T1, length(ts))
+    dϕ = zeros(T2, length(ϕs))
+    for i ∈ eachindex(dt)
+      t = (ts[i] - t0) * fs + 1
+      t̄ = floor(Int, t)
+      α, β = sincospi(0.5 * (t - t̄))
+      if t̄ ≤ n
+        dϕ[i] += β * dx[t̄]
+        dt[i] -= 0.5π * fs * α * ϕs[i] * dx[t̄]
+      end
+      if t̄ < n
+        dϕ[i] += α * dx[t̄+1]
+        dt[i] += 0.5π * fs  * β * ϕs[i] * dx[t̄+1]
+      end
+    end
+    return NoTangent(), dt, dϕ
+  end
+  return ir, _arr2ir_pullback
+end
+
+end # module
--- test/test_pekeris.jl
+++ test/test_pekeris.jl
@@ -110,8 +110,7 @@ end
   @test gradient(ℳ₁, AutoMooncake(config=nothing), x) ≈ ∇ℳ₁
   @test gradient(ℳ₁, AutoEnzyme(), x) ≈ ∇ℳ₁
   @test gradient(ℳ₂, AutoForwardDiff(), x) ≈ ∇ℳ₂
-  # FIXME: Zygote fails due to mutation in impulse_response()
-  # @test gradient(ℳ₂, AutoZygote(), x) ≈ ∇ℳ₂
+  @test gradient(ℳ₂, AutoZygote(), x) ≈ ∇ℳ₂
   @test gradient(ℳ₂, AutoMooncake(config=nothing), x) ≈ ∇ℳ₂
   @test gradient(ℳ₂, AutoEnzyme(), x) ≈ ∇ℳ₂
   x = [25.0, 200.0, 10.0, 8.0, 1000.0, 1540.0]
@@ -122,8 +121,7 @@ end
   @test gradient(ℳ₁, AutoMooncake(config=nothing), x) ≈ ∇ℳ₁
   @test gradient(ℳ₁, AutoEnzyme(), x) ≈ ∇ℳ₁
   @test gradient(ℳ₂, AutoForwardDiff(), x) ≈ ∇ℳ₂
-  # FIXME: Zygote fails due to mutation in impulse_response()
-  # @test gradient(ℳ₂, AutoZygote(), x) ≈ ∇ℳ₂
+  @test gradient(ℳ₂, AutoZygote(), x) ≈ ∇ℳ₂
   @test gradient(ℳ₂, AutoMooncake(config=nothing), x) ≈ ∇ℳ₂
   @test gradient(ℳ₂, AutoEnzyme(), x) ≈ ∇ℳ₂
 end

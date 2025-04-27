62057d364bdd61e88aceebbc7a27ad30c74c6ba2
Mandar Chitre <mandar@nus.edu.sg>
2025-04-27 16:29:46 +0800

    refactor(pm): make arrivals() type stable

--- src/api.jl
+++ src/api.jl
@@ -151,10 +151,14 @@ function acoustic_field(pm, tx, rxs::AbstractArray; kwargs...)
 end
 
 """
-    arrivals(pm, tx, rx)
+    arrivals(pm, tx, rx; paths=true)
 
 Compute the arrivals at the receiver `rx` due to the source `tx` using
 propagation model `pm`. Returns an array of arrivals.
+
+The eigenray paths are typically included in the arrivals. However, if they are
+not needed, one may set `paths=false` to allow the propagation model to avoid
+computing them.
 """
 function arrivals end
 

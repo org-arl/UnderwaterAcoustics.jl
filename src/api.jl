bc4e708f8409aeb0623ccff5e6d1fa757a4702a1
Mandar Chitre <mandar@nus.edu.sg>
2025-08-10 15:44:23 +0800

    doc: minor doc fixes + tut3 Turing API change

--- src/api.jl
+++ src/api.jl
@@ -156,9 +156,9 @@ end
 Compute the arrivals at the receiver `rx` due to the source `tx` using
 propagation model `pm`. Returns an array of arrivals.
 
-The eigenray paths are typically included in the arrivals. However, if they are
-not needed, one may set `paths=false` to allow the propagation model to avoid
-computing them.
+For ray models, eigenray paths are typically included in the arrivals.
+However, if they are not needed, one may set `paths=false` to allow the
+propagation model to avoid computing them.
 """
 function arrivals end
 
